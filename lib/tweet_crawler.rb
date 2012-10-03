726a71afabf57febe327a883a9b8fe007be5a937
Daniel Choi <dhchoi@gmail.com>
2012-10-02 20:20:17 -0400

    Show how to include Twitter search query in sample

--- lib/tweet_crawler.rb
+++ lib/tweet_crawler.rb
@@ -3,6 +3,9 @@ require 'sequel'
 require 'yaml'
 require 'pp'
 
+TWITTER_USER_FIELDS = %w(twitter_user_id name screen_name description location followers_count profile_image_url).map &:to_sym
+TWEET_FIELDS = %w(text retweet_count created_at).map &:to_sym
+
 config = YAML::load_file('config.yml')
 DB = Sequel.connect config['database']
 twitter_config = config['twitter']
@@ -14,18 +17,19 @@ Twitter.configure do |c|
   c.oauth_token_secret = twitter_config['access_token_secret']
 end
 
-twitter_config['lists'].each do |list|
+((twitter_config['searches'] || []) + (twitter_config['lists'] || [])).each do |search_or_list|
 
-  owner = list['owner']
-  slug = list['slug']
+  results = if (q = search_or_list['query'])
+    puts "-- Updating twitter search query: #{q}"
+    Twitter.search(q).statuses
+  else
+    owner = search_or_list['owner']
+    slug = search_or_list['slug']
+    puts "-- Updating twitter list: #{owner}#:#{slug}"
+    Twitter.list_timeline(owner, slug)
+  end
 
-  # api = "https://api.twitter.com/1/lists/statuses.xml?owner_screen_name=#{owner}&slug=#{slug}"
-
-  TWITTER_USER_FIELDS = %w(twitter_user_id name screen_name description location followers_count profile_image_url).map &:to_sym
-  TWEET_FIELDS = %w(text retweet_count created_at).map &:to_sym
-
-  Twitter.list_timeline(owner, slug).each {|tweet|
-
+  results.each {|tweet|
     u = tweet[:user].attrs
     u[:twitter_user_id] = u.delete(:id)
     params = u.select {|k,v| TWITTER_USER_FIELDS.member?(k)}
@@ -35,7 +39,7 @@ twitter_config['lists'].each do |list|
 
     if DB[:twitter_users].first(twitter_user_id:params[:twitter_user_id])
       DB[:twitter_users].filter(twitter_user_id:params[:twitter_user_id]).update params
-      puts "Updating user: #{params[:screen_name]}"
+      #puts "Updating user: #{params[:screen_name]}"
     else
       puts "Inserting #{params.inspect}"
       DB[:twitter_users].insert params
@@ -48,8 +52,12 @@ twitter_config['lists'].each do |list|
     x[:tweet_id] = tweet.id
     x[:twitter_user_id] = u[:twitter_user_id]
     x[:created_at] = Time.parse(x[:created_at]).localtime
-    unless  DB[:tweets].first(tweet_id:x[:tweet_id])
+    if (t =  DB[:tweets].first(tweet_id:x[:tweet_id]))
+      puts "Tweet already saved. #{t[:tweet_id]}: #{t[:text]}"
+      DB[:tweets].filter(tweet_id:x[:tweet_id]).update(x)
+    else 
       # Create tweet
+      puts "Saving tweet #{t[:tweet_id]}: #{t[:text]}"
       DB[:tweets].insert(x)
       puts "Inserting tweet: #{x['text']}"
     end
